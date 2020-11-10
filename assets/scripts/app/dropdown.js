fd560523466b3fb9a2ad6c4c3495a394c570b193
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-10 17:12:46 +0300

    fonts update local + dropdown bug [team] + dropdown bug [menu dark]

---
+++ assets/scripts/app/dropdown.js
@@ -0,0 +1,23 @@
+$('#navbarCol-1, #navbarCol-2').on('shown.bs.dropdown', function () {
+    $(this).parents('body').addClass('body_state_off');
+    if ($(".navbar").hasClass("navbar-light")) {
+      $('.navbar-light')
+        .addClass('navbar-dark')
+        .addClass('ohhimark')
+        .removeClass('navbar-light');
+      } else {
+
+      }
+    $(window).scrollTop(0);
+});
+
+ $('#navbarCol-1, #navbarCol-2').on('hide.bs.dropdown', function () {
+    $(this).parents('body').removeClass('body_state_off');
+    if ($(".navbar").hasClass("ohhimark")) {
+      $('.navbar-dark')
+        .addClass('navbar-light')
+        .removeClass('navbar-dark');
+      } else {
+
+      }
+});
