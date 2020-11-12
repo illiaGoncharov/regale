0392dd671cb8922d3e71c57f49e0dbe2995e9397
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-12 13:33:59 +0300

    dropdownphoto script local [all] + added some meta to index

---
+++ assets/scripts/app/dropdownphoto.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+  $('.dropdown__link-item:eq(0)').hover(function(){
+    $(".dropdown__pic").addClass("ser-1");
+  }, function () {
+    $(".dropdown__pic").removeClass("ser-1");
+  });
+  $('.dropdown__link-item:eq(1)').hover(function(){
+   $(".dropdown__pic").addClass("ser-2");
+  }, function () {
+    $(".dropdown__pic").removeClass("ser-2");
+  });
+});
