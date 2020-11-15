2f3c4c6bb5c2b4545b6702f24923a5912d189a90
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-15 14:08:36 +0300

    [index] mq-s + price .load footer, map, form + lots of minor mq updates + mobile menu  bug fixed

--- assets/scripts/app/dropdown.js
+++ assets/scripts/app/dropdown.js
@@ -1,4 +1,4 @@
-$('#navbarCol-1, #navbarCol-2').on('shown.bs.dropdown', function () {
+$('#navbarCol-1').on('shown.bs.dropdown', function () {
     $(this).parents('body').addClass('body_state_off');
     $(".content").addClass('zindexminus');
     if ($(".navbar").hasClass("navbar-light")) {
@@ -6,20 +6,30 @@ $('#navbarCol-1, #navbarCol-2').on('shown.bs.dropdown', function () {
         .addClass('navbar-dark')
         .addClass('ohhimark')
         .removeClass('navbar-light');
-      } else {
-
-      };
+      } else {};
     $(window).scrollTop(0);
 });
-
- $('#navbarCol-1, #navbarCol-2').on('hide.bs.dropdown', function () {
+$('#navbarCol-1').on('hide.bs.dropdown', function () {
     $(this).parents('body').removeClass('body_state_off');
     $(".content").removeClass('zindexminus');
     if ($(".navbar").hasClass("ohhimark")) {
       $('.navbar-dark')
         .addClass('navbar-light')
         .removeClass('navbar-dark');
-      } else {
+      } else {};
+    $('.dropdown__column').click(function(e) {
+        e.stopPropagation();
+    });
+});
 
-      };
+$('.navbar-toggler_user_Mobile').on('click', function() {
+  if ($("body").hasClass("body_state_off")) {
+    setTimeout(function() {
+      $('body').removeClass('body_state_off');
+      $(".content").removeClass('zindexminus');
+    }, 500);
+  } else {
+    $(this).parents('body').addClass('body_state_off');
+    $(".content").addClass('zindexminus');
+  }
 });
