6b89ddf0c16611b27466621fbc68c2a024cbd143
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-23 05:38:01 +0300

    [services] pages added

---
+++ assets/scripts/app/userowl.js
@@ -0,0 +1,29 @@
+$("#carousel").owlCarousel({
+      autoplay: true,
+      lazyLoad: true,
+      loop: true,
+      margin: 24,
+      /*animateOut: 'fadeOut',
+      animateIn: 'fadeIn',*/
+      /*responsiveClass: true,*/
+      autoHeight: false,
+      autoplayTimeout: 7000,
+      smartSpeed: 800,
+      responsive: {
+        0: {
+          items: 1
+        },
+
+        600: {
+          items: 1
+        },
+
+        1024: {
+          items: 3
+        },
+
+        1366: {
+          items: 4
+        }
+      }
+    });
