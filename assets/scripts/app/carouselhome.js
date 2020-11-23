6952c3e646e294b3f11e967d5ac1834f8e9c72c0
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-23 11:40:06 +0300

    [index] hot fix: controller && counter - header carousel && main-services

--- assets/scripts/app/carouselhome.js
+++ assets/scripts/app/carouselhome.js
@@ -1,12 +1,31 @@
-let totalItems = $('.header_carousel__carousel-item').length;
-let currentIndex = $('.header_carousel__carousel-item.active').index() + 1;
-$('.header_carousel__num-controller').html(''+currentIndex+'/'+totalItems+'');
+$(document).ready(function() {
+  $('#headerCarousel').carousel({
+      interval: 3500
+  });
+  let totalItems = $('.header_carousel__carousel-item').length;
+  let currentIndex = $('.header_carousel__carousel-item.active').index() + 1;
+  $('.header_carousel__num-controller').html(''+currentIndex+'/'+totalItems+'');
 
-$('#headerCarousel').carousel({
-    interval: 3500
+  $('#headerCarousel').on('slid.bs.carousel', function(e) {
+    currentIndex = $('.header_carousel__carousel-item.active').index() + 1;
+    $('.header_carousel__num-controller').html(''+currentIndex+'/'+totalItems+'');
+  });
 });
 
-$('#headerCarousel').on('slide.bs.carousel', function() {
-  currentIndex = $('.header_carousel__carousel-item.active').index() + 1;
- $('.header_carousel__num-controller').html(''+currentIndex+'/'+totalItems+'');
+$(document).ready(function() {
+  $('#mainServCarousel').carousel({
+      interval: 4000
+  });
+  let totalItems = $('.main-services__module').length;
+  let currentIndex = $('.main-services__module.active').index() + 1;
+  $('.main-services__num-controller').html(''+currentIndex+'/'+totalItems+'');
+
+  $('#mainServCarousel').on('slid.bs.carousel', function(e) {
+    currentIndex = $('.main-services__module.active').index() + 1;
+    $('.main-services__num-controller').html(''+currentIndex+'/'+totalItems+'');
+  });
 });
+
+
+
+
