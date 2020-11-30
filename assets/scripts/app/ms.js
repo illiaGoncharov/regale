d1514f4da3bea60e7023283299e7e2aad203b9cb
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-11-30 06:24:05 +0300

    [index] ms upd-s: photos 6 blue + [filials] && map upd-s: API added + [filials] && map upd-s + add markers

--- assets/scripts/app/ms.js
+++ assets/scripts/app/ms.js
@@ -1,5 +1,5 @@
 let mCar = $('#mainServCarousel');
 $(mCar).bind('slid.bs.carousel', function (e) {
-  var randomNum = Math.floor((Math.random()*10)+1);
-  $('.main-services__img').attr('src', 'images/img-' + randomNum + '.jpg');
+  var randomNum = Math.floor((Math.random()*6)+1);
+  $('.main-services__img').attr('src', 'blocks/main-services/__img/main-services-img-' + randomNum + '.jpg');
 });
