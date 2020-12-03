31702b961fe34e234a6f27687a074ddcf2a39775
Ilia Goncharov <ilia.m.goncharov@gmail.com>
2020-12-03 15:41:36 +0300

    form php sender upd (test)

---
+++ assets/scripts/app/form.js
@@ -0,0 +1,26 @@
+$('form').submit(function() {
+    // для читаемости кода
+    var $form = $(this);
+
+    // чистим ошибки
+    $form.find('.error').remove();
+
+    // проверяем поле с именем пользователя
+    if ($form.find('input[name=firstname]').val() === '') {
+        // добавляем текст ошибки
+        $form.find('input[name=firstname]')
+          .before('<div class="error">Введите имя</div>');
+        // прерываем дальнейшую обработку
+        return false;
+    }
+
+    // всё хорошо – отправляем запрос на сервер
+    $.post(
+        $form.attr('action'), // ссылка куда отправляем данные
+        $form.serialize()     // данные формы
+    );
+
+    // отключаем действие по умолчанию
+    $('#formModal').modal('show');
+    return false;
+});
